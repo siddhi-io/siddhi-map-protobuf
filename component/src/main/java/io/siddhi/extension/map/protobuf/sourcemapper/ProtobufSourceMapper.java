41ac5cf611b7e98d0bda6ae7440046544b40104c
Amstrom <sahandilshansg@gmail.com>
2019-09-23 11:41:17 +0530

    Re-update the link to the 'sample.proto' file

--- component/src/main/java/io/siddhi/extension/map/protobuf/sourcemapper/ProtobufSourceMapper.java
+++ component/src/main/java/io/siddhi/extension/map/protobuf/sourcemapper/ProtobufSourceMapper.java
@@ -61,7 +61,7 @@ import static io.siddhi.extension.map.protobuf.utils.ProtobufUtils.protobufField
                 "input mapper, you can either define stream attributes as the same names as the protobuf message " +
                 "attributes or you can use custom mapping to map stream definition attributes with the protobuf " +
                 "attributes..Please find the sample proto definition [here](https://github.com/siddhi-io/siddhi-map-" +
-                "protobuf/tree/master/component/src/main/resources) ",
+                "protobuf/tree/master/component/src/main/resources/sample.proto) ",
         parameters = {
                 @Parameter(name = "class",
                         description = "" +
